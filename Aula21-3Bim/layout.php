1386c7ddfd097b26c7a685f9698c7a9a90f15c8a
OrestesFerraz <148365065+OrestesFerraz@users.noreply.github.com>
2024-08-27 16:51:49 -0300

    Add files via upload

---
+++ Aula21-3Bim/layout.php
@@ -0,0 +1,37 @@
+
+    <?php
+        include 'header.php';
+    ?>
+
+    <div class="container-fluid">
+        <div class="row">
+            
+            <?php 
+                include 'nav.php';
+            ?>
+
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 class="h2">Início</h1>
+                </div>
+                <br>
+                <p class="display-4">
+                    Seja bem vindo a aplicação <strong>"CRUD PHP"</strong>.
+                </p>
+                <p class="display-4">
+                    Esta é a página inicial.
+                </p>
+            </main>
+        </div>
+    </div>
+
+    <?php 
+        include 'footer.php';
+    ?>
+
+    <script src="dist/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
+    <script src="dist/dashboard.js"></script>
+</body>
+
+</html>
---
+++ Aula21-3Bim/nav.php
@@ -0,0 +1,24 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                <div class="position-sticky mt-3">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="layout.php">
+                                <span data-feather="home"></span>
+                                Início
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="formulario.php">
+                                <span data-feather="file-text"></span>
+                                Formulário
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="listagem.php">
+                                <span data-feather="list"></span>
+                                Listagem
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
